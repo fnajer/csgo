8f369c4e3fe638092ef07075083fe445f9a81121
fnajer <lexactr@gmail.com>
2019-08-03 16:55:31 +0300

    add icons for header and chat modules

---
+++ src/shared/icons/chat/send.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Icon = props => (
+  <svg {...props} className={props.className} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 40 40">
+    <path d="M640,4924a2.231,2.231,0,0,1-1.228,1.99l-35.552,17.78a2.225,2.225,0,0,1-2.879-3.17l7.532-12.05L620,4924l-12.129-4.55-7.532-12.05a2.22,2.22,0,0,1,2.879-3.16l35.552,17.77A2.231,2.231,0,0,1,640,4924Z" transform="translate(-600 -4904)"/>
+  </svg>
+)
+
+export default Icon
+
